7181c95c00af2b7a74013d03d8762988a732309a
Yovan Naumovski <jovan3.14@gmail.com>
2018-02-04 11:41:23 +0200

    cleanup

--- lg_remote.ino
+++ lg_remote.ino
@@ -48,20 +48,15 @@ void sendIR(long irSignal) {
 int checkButtonState(int buttonState, int lastButtonState, int buttonPin, unsigned long irSignal) {
   buttonState = digitalRead(buttonPin);
 
-  // compare the buttonState to its previous state
   if (buttonState != lastButtonState) {
-    // if the state has changed, increment the counter
     if (buttonState == HIGH) {
-      // if the current state is HIGH then the button went from off to on:
       Serial.println("button ON " + buttonPin);
       sendIR(irSignal);
     } else {
-      // if the current state is LOW then the button went from on to off:
       Serial.println(buttonPin + " off");
     }
-    // Delay a little bit to avoid bouncing
     delay(50);
   }
-  // save the current state as the last state, for next time through the loop
+
   return buttonState;
 }
